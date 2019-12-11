9cbc2a87999eee2f9290e1daa0b1a91cab01b5c9
SergioBarretoJr <49816375+SergioBarretoJr@users.noreply.github.com>
2019-12-10 22:28:53 -0300

    Update model.py

--- model.py
+++ model.py
@@ -4,38 +4,31 @@ import numpy as np
 import pandas as pd
 from gensim.models import doc2vec
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 from gensim.models.doc2vec import Doc2Vec
 from sklearn.preprocessing import StandardScaler
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from sklearn.model_selection import cross_val_score
 
 import codecs
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def read_dataset(path_train,path_test):
-    dataset_trains = pd.read_csv(path_train, header=0, delimiter=";")
-    dataset_test = pd.read_csv(path_test, header=0, delimiter=";")
-    dataset_trains=pd.DataFrame(dataset_trains,columns=['0','1'])
-    dataset_test = pd.DataFrame(dataset_test, columns=['0', '1'])
-
-    x_train=dataset_trains['0']
-    y_train = dataset_trains['1']
-    x_test = dataset_test['0']
-    y_test = dataset_test['1']
+def read_dataset(path):
+    dataset = pd.read_csv(path, header=0, delimiter="\t")
+    x_train, x_test, y_train, y_test = train_test_split(dataset.review, dataset.sentiment, random_state=0, test_size=0.2)
     x_train = label_sentences(x_train, 'Train')
     x_test = label_sentences(x_test, 'Test')
     all_data = x_train + x_test
     return x_train, x_test, y_train, y_test, all_data
 
 def label_sentences(corpus, label_type):
-
+    """
+    Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
+    We do this by using the LabeledSentence method. The format will be "TRAIN_i" or "TEST_i" where "i" is
+    a dummy index of the review.
+    """
     labeled = []
     for i, v in enumerate(corpus):
         label = label_type + '_' + str(i)
@@ -43,24 +36,29 @@ def label_sentences(corpus, label_type):
     return labeled
 
 def get_vectors(doc2vec_model, corpus_size, vectors_size, vectors_type):
+    """
+    Get vectors from trained doc2vec model
+    :param doc2vec_model: Trained Doc2Vec model
+    :param corpus_size: Size of the data
+    :param vectors_size: Size of the embedding vectors
+    :param vectors_type: Training or Testing vectors
+    :return: list of vectors
+    """
     vectors = np.zeros((corpus_size, vectors_size))
     for i in range(0, corpus_size):
         prefix = vectors_type + '_' + str(i)
         vectors[i] = doc2vec_model.docvecs[prefix]
     return vectors
 
-def train_doc2vec(corpus,w,e,Vs):
+def train_doc2vec(corpus):
     logging.info("Building Doc2Vec vocabulary")
-    d2v = doc2vec.Doc2Vec(min_count=1,  #Ignores all words with total frequency lower than this
-                          window=w,  # The maximum distance between the current and predicted word within a sentence
-                          vector_size=Vs,  # Dimensionality of the generated feature vectors
+    d2v = doc2vec.Doc2Vec(min_count=5,  #Ignores all words with total frequency lower than this
+                          window=5,  # The maximum distance between the current and predicted word within a sentence
+                          vector_size=300,  # Dimensionality of the generated feature vectors
                           workers=5,  # Number of worker threads to train the model
-                          alpha=0.01,  # The initial learning rate
+                          alpha=0.025,  # The initial learning rate
                           min_alpha=0.00025,  # Learning rate will linearly drop to min_alpha as training progresses
-                          dbow_words=0,
-                          hs=0,
-                          seed=123,
-                          dm=0,)  # dm=1 means 'distributed memory' (PV-DM:predict a center word from the randomly
+                          dm=1)  # dm=1 means 'distributed memory' (PV-DM:predict a center word from the randomly
                                                                         # sampled set of words by taking as input — 
                                                                         # the context words and a paragraph id.)
                                  # and dm =0 means 'distributed bag of words' (PV-DBOW: ignores the context words in
@@ -70,7 +68,7 @@ def train_doc2vec(corpus,w,e,Vs):
     d2v.build_vocab(corpus)
 
     logging.info("Training Doc2Vec model")
-    for epoch in range(e):
+    for epoch in range(5):
         logging.info('Training iteration #{0}'.format(epoch))
         d2v.train(corpus, total_examples=d2v.corpus_count, epochs=d2v.epochs)
         # shuffle the corpus
@@ -83,49 +81,71 @@ def train_doc2vec(corpus,w,e,Vs):
     d2v.save("d2v.model")
     return d2v
 
-def train_classifier(d2v, training_vectors, training_labels,Vs):
+def train_classifier(d2v, training_vectors, training_labels):
     logging.info("Classifier training")
-    train_vectors = get_vectors(d2v, len(training_vectors), Vs, 'Train')
-    model = KNeighborsClassifier()
+    train_vectors = get_vectors(d2v, len(training_vectors), 300, 'Train')
+    model = LogisticRegression()
     model.fit(train_vectors, np.array(training_labels))
+    training_predictions = model.predict(train_vectors)
+    logging.info('Training predicted classes: {}'.format(np.unique(training_predictions)))
+    logging.info('Training accuracy: {}'.format(accuracy_score(training_labels, training_predictions)))
+    logging.info('Training F1 score: {}'.format(f1_score(training_labels, training_predictions, average='weighted')))
     return model
 
-def test_classifier(d2v, classifier, testing_vectors, testing_labels,Vs,cv):
+def test_classifier(d2v, classifier, testing_vectors, testing_labels):
     logging.info("Classifier testing")
-    test_vectors = get_vectors(d2v, len(testing_vectors), Vs, 'Test')
-    scores=cross_val_score(classifier, test_vectors, testing_labels, cv=cv)
-    print(scores)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    test_vectors = get_vectors(d2v, len(testing_vectors), 300, 'Test')
+    testing_predictions = classifier.predict(test_vectors)
+    logging.info('Testing predicted classes: {}'.format(np.unique(testing_predictions)))
+    logging.info('Testing accuracy: {}'.format(accuracy_score(testing_labels, testing_predictions)))
+    logging.info('Testing F1 score: {}'.format(f1_score(testing_labels, testing_predictions, average='weighted')))
 
-def build_PVDBOW(input_trains1,input_test1, w1,e1,cv,plot_Class=False):
-    input_trains=input_trains1
-    input_test = input_test1
-    dataset = pd.read_csv(input_trains, header=0, delimiter=";")
-    dataset_test= pd.read_csv(input_test, header=0, delimiter=";")
-    dataset = pd.DataFrame(dataset, columns=['0', '1'])
-    dataset_test=pd.DataFrame(dataset_test, columns=['0', '1'])
-    if (plot_Class):
-        print("Descrição da Base:")
-        print("%Positivo: ",np.mean(dataset['1']))
-        print("%Positivo validação: ",np.mean(dataset_test['1']))
-        sns.catplot(x="1", kind="count", data=dataset,color='blue')
-        sns.catplot(x="1", kind="count", data=dataset_test, color='black')
-        plt.show()
+def predict_Class(d2v, classifier, strings):
+    logging.info("Classifing new instance")
+    vec = doc2vec.infer_vector(doc_words=strings.split(), alpha=0.025, steps=20)
+    if (classifier.predict(vec.reshape(1, -1))==1):
+        print("Avaliação positiva\n")
+    else: print("Avaliação negativa\n")
+
+
+def view_Embeddings(d2v_model,doc,path_vec,LearnR,epoch):
+    test_docs = doc
+    test_docs = [x.strip().split() for x in codecs.open(test_docs, "r", "utf-8").readlines()]
+    output = open(path_vec, "w")
+    vec_docs=[]
+    for d in test_docs:
+        output.write(" ".join([str(x) for x in d2v_model.infer_vector(d, alpha=LearnR, steps=epoch)]) + "\n")
+        vec_docs.append(d2v_model.infer_vector(d, alpha=LearnR, steps=epoch))
+    output.flush()
+    output.close()
+    return vec_docs
+
+
+if __name__ == "__main__":
 
     #Separando treinamento, teste, classe
-    x_train, x_test, y_train, y_test, all_data = read_dataset(input_trains,input_test)
-    print("# de tweets: ",len(x_train)+len(x_test))
-    print("# de tweets treino: ",len(x_train))
-    print("# de tweets test: ", len(x_test))
-    print("% de tweets test: ", len(x_test)*100/(len(x_train)+len(x_test)))
-    w=w1
-    e=e1
+    x_train, x_test, y_train, y_test, all_data = read_dataset('dataset.csv')
+
     #construindo a representaçao embedding
-    doc2vec = train_doc2vec(all_data,w,e)
+    #doc2vec = train_doc2vec(all_data)
+
+    #carregando modelo criado
+    doc2vec = Doc2Vec.load("d2v.model")
+
+    #salvar representação vetorial da base
+    #text_vec=view_Embeddings(doc2vec,'dataset.csv','dataset_vectors.txt', 0.025, 3)
+    #print(text_vec)
 
     #usando a representação vetorial para treinar classificador
     classifier = train_classifier(doc2vec, x_train, y_train)
 
     #testando classificador gerado
-    test_classifier(doc2vec, classifier, x_test, y_test,cv)
+    test_classifier(doc2vec, classifier, x_test, y_test)
+
+
+    # testando nova avaliação de filme
+    strings = 'Avengers is a very good film. i loved it so much'
+    string2 = 'Avengers is the worst film. i hated it'
+    predict_Class(doc2vec, classifier, strings)
+    predict_Class(doc2vec, classifier, string2)
 
